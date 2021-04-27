101a15f883edbd99174756a0fe43779687a805c9
jn-mic <jackotienokey@gmail.com>
2021-04-27 23:51:54 +0300

    adding more file on the project

---
+++ js/gvb.js
@@ -0,0 +1,15 @@
+
+
+
+
+$(document).ready(function(event){
+    event.preventDefault;
+    $(".navbar").click(function{
+        $(".navbar").hide();
+        $(".navbar").show();
+    });
+    $("#vila").hover( function(){
+        $("#vila").hide();
+    });
+
+});
